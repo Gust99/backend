ab7f6cf6edf101ca6e928aaeaee2c687bd3db62d
gust99 <Gustavo-Gudino@jala-fundation.org>
2022-06-25 11:25:02 -0400

    design patterns, example

--- DESIGN PATTERNS/index.ts
+++ DESIGN PATTERNS/index.ts
@@ -3,6 +3,7 @@ import FactoryB from './Factory/Factories/FactoryB';
 import GenericFactory from './Factory/Factories/GenericFactory';
 import ProductA from './Factory/Products/ProductA';
 import ProductB from './Factory/Products/ProductB';
+import IProduct from './Factory/Products/IProduct';
 
 // npm init @eslint/config
 // https://eslint.org/docs/latest/user-guide/getting-started
@@ -15,8 +16,8 @@ const factoryB = new FactoryB();
 const productB = factoryB.createProduct();
 productB.build();
 
-const a2 = GenericFactory.createProduct(ProductA);
-const b2 = GenericFactory.createProduct(ProductB);
+const a2 = GenericFactory.createProduct<IProduct>(ProductA); // Type of IProduct
+const b2 = GenericFactory.createProduct(ProductB); // Type of ProductB
 
 a2.build();
 b2.build();
